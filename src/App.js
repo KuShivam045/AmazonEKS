2e647eab6f26bc21bed178fcce815f39c9c64d1c
shivam kumar <kumarshivam9318@gmail.com>
2024-10-10 18:31:10 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <div className="card">
+      <img 
+        src="https://via.placeholder.com/300" 
+        alt="Placeholder" 
+        className="card-image" 
+      />
+      <div className="card-content">
+        <h2>Card Title</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is some dummy text for the card description. You can add more
+          content here to make it more descriptive.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+    </div>
     </div>
   );
 }
